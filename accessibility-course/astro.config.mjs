4ec5409af26ec83f8584fe70463cc7250220b1ff
sammieatkins <atk21007@byui.edu>
2025-02-25 19:41:03 -0700

    darn pre code block won't do what i want for the colors but whatever lol good luck future me

--- accessibility-course/astro.config.mjs
+++ accessibility-course/astro.config.mjs
@@ -1,6 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { createCssVariablesTheme } from "shiki/core";
+
+const myTheme = createCssVariablesTheme({
+  name: "dark-plus-custom",
+  variablePrefix: "--shiki-",
+});
 
 export default defineConfig({
   integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: myTheme,
+      wrap: true,
+    },
+  },
 });
--- accessibility-course/tailwind.config.mjs
+++ accessibility-course/tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
-    theme: {
-      extend: {},
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'], // Set Nunito as the default sans-serif font
+        nunito: ['Nunito', 'sans-serif'], // Explicitly define "font-nunito" class
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
