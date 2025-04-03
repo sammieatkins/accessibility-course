983910bc8e57622170dcd6f50e32a4f4ad640ede
sammieatkins <atk21007@byui.edu>
2025-04-03 11:13:28 -0600

    get grouped lessons might have been the culprit??

--- accessibility-course/src/utils/getGroupedLessons.ts
+++ accessibility-course/src/utils/getGroupedLessons.ts
@@ -1,9 +1,7 @@
 import { getCollection } from "astro:content";
 import type Lesson from "../types/Lesson";
 
-export async function getGroupedLessons(): Promise<
-  { index: number; folder: string; lessons: Lesson[] }[]
-> {
+export async function getGroupedLessons(): Promise<[string, Lesson[]][]> {
   const lessons = await getCollection("lessons");
 
   const groupedLessons = lessons.reduce((groups, lesson) => {
@@ -16,7 +14,7 @@ export async function getGroupedLessons(): Promise<
   }, {} as Record<string, Lesson[]>);
 
   const sortedGroups = Object.entries(groupedLessons).map(
-    ([folder, lessons], index) => {
+    ([folder, lessons]) => {
       const sortedLessons = lessons.sort((a, b) => {
         const aType = a.data.type || "";
         const bType = b.data.type || "";
@@ -27,11 +25,7 @@ export async function getGroupedLessons(): Promise<
         return (a.data.order ?? 100) - (b.data.order ?? 100);
       });
 
-      return {
-        index: index + 1,
-        folder,
-        lessons: sortedLessons,
-      };
+      return [folder, sortedLessons] as [string, Lesson[]];
     }
   );
 
